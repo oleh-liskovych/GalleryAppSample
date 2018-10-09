dcd308c663a0f927b008dbd2c79d7b3b8bf42964
Oleg  Liskovich <oleg.l@powercode.us>
2018-10-09 12:23:39 +0300

    Parcelable fix

--- app/src/main/java/com/olehliskovych/picturesgallerysampleapp/data/entity/SearchResultEntity.kt
+++ app/src/main/java/com/olehliskovych/picturesgallerysampleapp/data/entity/SearchResultEntity.kt
@@ -1,34 +1,9 @@
 package com.olehliskovych.picturesgallerysampleapp.data.entity
 
-import android.os.Parcel
 import android.os.Parcelable
 import com.google.gson.annotations.Expose
+import kotlinx.android.parcel.Parcelize
 
+@Parcelize
 data class SearchResultEntity(@Expose val total: Int,
-                              @Expose val results: List<PictureEntity>) : Parcelable {
-
-    constructor(parcel: Parcel) : this(
-            parcel.readInt(),
-            parcel.createTypedArrayList(PictureEntity.CREATOR)) {
-    }
-
-    override fun writeToParcel(parcel: Parcel, flags: Int) {
-        parcel.writeInt(total)
-        parcel.writeTypedList(results)
-    }
-
-    override fun describeContents(): Int {
-        return 0
-    }
-
-    companion object CREATOR : Parcelable.Creator<SearchResultEntity> {
-        override fun createFromParcel(parcel: Parcel): SearchResultEntity {
-            return SearchResultEntity(parcel)
-        }
-
-        override fun newArray(size: Int): Array<SearchResultEntity?> {
-            return arrayOfNulls(size)
-        }
-    }
-
-}
+                              @Expose val results: List<PictureEntity>) : Parcelable
